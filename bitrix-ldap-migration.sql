45d585aa5a0f170144fa0ebc4f8774afddff0235
Azhinu <ari@ariellab.tk>
2021-04-22 19:32:40 +0300

    Added Bitrix LDAP Migration SQL Query

---
+++ bitrix-ldap-migration.sql
@@ -0,0 +1,15 @@
+SET @users := 'user1,user2';
+
+DELETE t1 FROM b_user t1
+INNER  JOIN b_user t2
+WHERE
+    t1.id > t2.id AND
+    t1.login = t2.login;
+
+UPDATE b_user
+SET EXTERNAL_AUTH_ID = 'LDAP#id'
+WHERE FIND_IN_SET(b_user.login, @users);
+
+SELECT ID,LOGIN,EXTERNAL_AUTH_ID
+FROM b_user
+WHERE FIND_IN_SET(b_user.login, @users);
