6da6d6a1e0e1355c1f2bc9cf38fba9372ed8055c
Azhinu <ari@ariellab.tk>
2021-05-09 00:50:21 +0300

    Updated NIC failover service

--- dual-nic-reservation/nic-reservation.sh
+++ dual-nic-reservation/nic-reservation.sh
@@ -1,34 +1,58 @@
 #!/usr/bin/env bash
 
+#Config vars:
+declare -A  NIC_primary=( ["gateway"]="" ["iface"]="" ["weight"]= )
+declare -A  NIC_secondary=( ["gateway"]="" ["iface"]="" ["weight"]= )
+idle_timeout=30
+retry_timeout=60
+
+
 healthcheck() {
-  local interfaces result
-  for i in $(ip route | grep -oP '(?<=dev )\w+(?= \w+ \d $)'); do
-    echo "Found inteface: $i"
-    interfaces+=("$i")
-  done
+  local result ifdead=false
+  #Get NICs list
+  # for i in $(ip route | grep -oP '(?<=dev )\w+(?= \w+ \d $)'); do
+  #   echo "Found inteface: $i"
+  #   interfaces+=("$i")
+  # done
 
   while true; do
 
-    for i in interfaces; do
-      result=$(ping -c 1 -I $i 1.1.1.1 | tail -n 2)
-      echo "$result"
-      if [[ $(echo $result | grep -oP \d(?= received)) -eq 0 ]]; then
-        ifdown $i
-        echo -e "Inteface $i has no internet connection! NIC has been disabled.\n Waiting for retry"
-
+    for iface in ${NIC_primary[iface]} ${NIC_secondary[iface]}; do
+      #Ping CloudFlare
+      result=$(ping -c 1 -I ${iface} 1.1.1.1 | sed -n 5p)
+      #Check if no package received
+      if [[ $(echo $result | grep -oP '\d(?= received)') -eq 0 ]]; then
+        #Check is primary of secondary interface dead
+        #Set live inteface higher priority
+        if [[ $iface -eq ${NIC_primary[iface]} ]]; then
+          ip route replace default scope global \
+            nexthop via ${NIC_primary[gateway]} dev ${NIC_primary[iface]} weight 100 \
+            nexthop via ${NIC_secondary[gateway]} dev ${NIC_secondary[iface]} weight 1
+        else
+          ip route replace default scope global \
+            nexthop via ${NIC_primary[gateway]} dev ${NIC_primary[iface]} weight 1 \
+            nexthop via ${NIC_secondary[gateway]} dev ${NIC_secondary[iface]} weight 100
+        fi
+        echo -e "Inteface $iface has no internet connection! NIC has been disabled.\n Waiting for retry"
+        ifdead=true
+        while [[ $ifdead ]]; do
+          sleep $retry_timeout
+          #Ping CloudFlare
+          result=$(ping -c 1 -I ${iface} 1.1.1.1 | sed -n 5p)
+          #Check if package received
+          if [[ $(echo $result | grep -oP \d(?= received)) -eq 1 ]]; then
+            ifdead=false
+            ip route replace default scope global \
+              nexthop via ${NIC_primary[gateway]} dev ${NIC_primary[iface]} weight ${NIC_primary[weight]} \
+              nexthop via ${NIC_secondary[gateway]} dev ${NIC_secondary[iface]} weight ${NIC_secondary[weight]}
+            echo "Interface $iface alive! Restoring routes."
+          fi
+        done
       fi
     done
-
-
-    sleep 30
+    sleep $idle_timeout
   done
-
 }
-
-declare -A  NIC_primary=( ["gateway"]="" ["iface"]="" ["weight"]= )
-declare -A  NIC_secondary=( ["gateway"]="" ["iface"]="" ["weight"]= )
-
-
 
 case $1 in
   "up")
