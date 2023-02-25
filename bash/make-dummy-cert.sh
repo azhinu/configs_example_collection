e3ce27a8d4a37ff4294daa11e5669b4cd05fea54
azhinu <ari@ariellab.tk>
2023-02-25 22:13:18 +0300

    Now ED25519

--- bash/make-dummy-cert.sh
+++ bash/make-dummy-cert.sh
@@ -20,11 +20,12 @@ for target in "$@" ; do
 	PEM1=`/bin/mktemp /tmp/openssl.XXXXXX`
 	PEM2=`/bin/mktemp /tmp/openssl.XXXXXX`
 	trap "rm -f $PEM1 $PEM2" INT
-	answers | /usr/bin/openssl req -newkey rsa:2048 -keyout $PEM1 -nodes -x509 -days 365 -out $PEM2 2> /dev/null
+	answers | /usr/bin/openssl req -newkey ED25519 -keyout $PEM1 -nodes -x509 -days 365 -out $PEM2 2> /dev/null
 	cat $PEM1 >  ${target}
 	echo ""   >> ${target}
 	cat $PEM2 >> ${target}
 	rm -f $PEM1 $PEM2
 done
 
+#openssl req -newkey ED25519 -nodes -x509 -days 365 -out cert.pem
 #openssl req -newkey rsa:2048 -nodes -x509 -days 365 -out cert.pem
